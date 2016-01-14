78ea53d4a714cf3529f7d0e25473eef0b2ead57f
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2016-01-14 16:06:42 +0100

    Initial 'rails-api' file structure [finished #111685414]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+BucketlistApi::Application.config.secret_token = '83b8ec28c5b155dbfd9183ae8188316d909429293ec2ffd330077ddb451ff1ad2b803be42829da0db6dca2b2d8de83cf6ae567d31befde426b4247e151c89dca'
