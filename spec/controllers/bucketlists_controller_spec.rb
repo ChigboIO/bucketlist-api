66aa7e644249d9cd7d1ecbe40f709fe08c9e7364
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2016-01-15 14:42:52 +0100

    Implement show bucket '/bucketlist/:id'
    Why?
    To get a articular bucketlist
    The API consumer can make a get request to /bucketlist/:id
    And get the bucketlist with the specified ID
    
    How?
    Define method 'show' in the BucketlistController
    Set the method to find the bucketlist with the
    Return the bucketlist if found
    Or return custome 'Not Found' error message
    
    Completing https://www.pivotaltracker.com/story/show/111717950
    [finished #111717950]

--- spec/controllers/bucketlists_controller_spec.rb
+++ spec/controllers/bucketlists_controller_spec.rb
@@ -2,10 +2,26 @@ require "rails_helper"
 
 RSpec.describe Api::BucketlistsController, type: :controller do
   describe "GET index" do
-    it "assigns @bucketlist" do
+    it "assigns @bucketlists and return http_status 200" do
       bucketlists = create(:bucketlist)
       get :index
       expect(assigns(:bucketlists)).to eq([bucketlists])
+      expect(response).to have_http_status(200)
+      # expect(JSON.parse(response.body)).to be_valid
+    end
+  end
+
+  describe "GET show" do
+    it "assigns @bucketlist and return http_status 200" do
+      bucketlist = create(:bucketlist)
+      get :show, id: bucketlist.id
+      expect(assigns(:bucketlist)).to eq(bucketlist)
+      expect(response).to have_http_status(200)
+    end
+
+    it "and return http_status 404" do
+      get :show, id: 20
+      expect(response).to have_http_status(404)
     end
   end
 end
